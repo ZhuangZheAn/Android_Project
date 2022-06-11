1652e7ce0dbc6a58a521bfa614449bdf1778f40d
Yu-Wei, Lyu <awaia732@gmail.com>
2022-06-11 19:20:59 +0800

    優化新增介面、資料處理與顯示

--- Project/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Project/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -1,29 +1,48 @@
 package com.example.android.recyclerview;
 
+import static java.util.Arrays.sort;
+
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     // Tag for the intent extra.
     public static final String EXTRA_MESSAGE = "com.example.android.droidcafeoptions.extra.MESSAGE";
 
-    // The order message, displayed in the Toast and sent to the new Activity.
-    private String mOrderMessage;
 
     /*test*/
     private final LinkedList<String> mWordList = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private WordListAdapter mAdapter;
+    private static final String DATASIZE_KEY = "DataSize";
+    private static final String DATAS_KEY = "Datas";
+    private static final String SPLIT_CHAR = "!@";
+    private static final String SPLIT_CHAR2 = "#$";
+    private static final String NEW_KEY = "New";
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile =
+            "com.example.android.recyclerview";
+    private String datas;
+    private Integer data_size;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,9 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        for (int i = 0; i < 3; i++) {
-            mWordList.addLast("Word " + i);
-        }
+
         // Create recycler view.
         mRecyclerView = findViewById(R.id.recyclerview);
         // Create an adapter and supply the data to be displayed.
@@ -53,5 +70,81 @@ public class MainActivity extends AppCompatActivity {
         // Give the recycler view a default layout manager.
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        Intent intent = getIntent();
+        String req = intent.getStringExtra(MainActivity.NEW_KEY);
+        data_size = mPreferences.getInt(DATASIZE_KEY,0);
+        datas = mPreferences.getString(DATAS_KEY,"");
+        if(req != null){
+            data_size = mPreferences.getInt(DATASIZE_KEY,0);
+            data_size += 1;
+
+            String[] arr = new String[data_size];
+            if(datas == ""){
+                datas = req;
+                arr[0] = req;
+            }
+            else{
+                datas += SPLIT_CHAR + req;
+                arr = datas.split(SPLIT_CHAR);
+                sort(arr);
+            }
+            for(int i = 0; i < data_size; i++){
+                mWordList.addLast(arr[i].replace(SPLIT_CHAR2, " "));
+            }
+            SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+            preferencesEditor.putInt(DATASIZE_KEY, data_size);
+            preferencesEditor.putString(DATAS_KEY, datas);
+            preferencesEditor.apply();
+        }
+        else{
+            if(data_size != 0){
+                String[] arr = datas.split(SPLIT_CHAR);
+                sort(arr);
+                for(int i = 0; i < data_size; i++){
+                    mWordList.addLast(arr[i].replace(SPLIT_CHAR2, " "));
+                }
+            }
+        }
+        //makeToast(Integer.toString(data_size);
+
+    }
+
+    public void reset(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("警告!!");
+        builder.setMessage("這個動作會刪除所有已儲存的資料");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton(
+                "取消",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        makeToast("取消操作");
+                        dialog.cancel();
+                    }
+                });
+
+        builder.setNegativeButton(
+                "確定",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+                        preferencesEditor.clear();
+                        preferencesEditor.apply();
+                        mWordList.clear();
+                        mRecyclerView.setAdapter(mAdapter);
+                        makeToast("已刪除所有資料");
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void makeToast(String message){
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
     }
 }
--- Project/app/src/main/java/com/example/android/recyclerview/SecondActivity.java
+++ Project/app/src/main/java/com/example/android/recyclerview/SecondActivity.java
@@ -1,29 +1,50 @@
 package com.example.android.recyclerview;
 
-import android.app.DialogFragment;
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v4.app.LoaderManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class SecondActivity extends AppCompatActivity{
 
     private Spinner mSpinner;
+    private TextView mTextviewTime;
+    private EditText mPurposeMTV;
+    private EditText mCostMTV;
+    private EditText mExMTV;
+    private String month_string;
+    private String day_string;
+    private String year_string;
+    private String hour_string;
+    private String minute_string;
+    private String second_string;
+    private String timeMessage;
+    private static final String NEW_KEY = "New";
+    private static final String SPLIT_CHAR2 = "#$";
 
+    @SuppressLint("SimpleDateFormat")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
+        mPurposeMTV = findViewById(R.id.purposeMTV);
+        mCostMTV = findViewById(R.id.costMTV);
+        mExMTV = findViewById(R.id.exMTV);
+        mTextviewTime = findViewById(R.id.tvTime);
+        GetNowTime();
         mSpinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.http_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,39 +52,73 @@ public class SecondActivity extends AppCompatActivity{
     }
 
     public void ClickFinish(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        String purpose = mPurposeMTV.getText().toString();
+        String cost = mCostMTV.getText().toString();
+        String ex = mExMTV.getText().toString();
+        if(Objects.equals(purpose, "") || Objects.equals(cost, "")){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("提醒");
+            builder.setMessage("目的與金額不可留空");
+            builder.setCancelable(true);
+            builder.setPositiveButton(
+                    "我知道了",
+                    (dialog, id) -> dialog.cancel());
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        else{
+            String extra = timeMessage + "\n" + purpose + SPLIT_CHAR2 + cost + SPLIT_CHAR2 + ex;
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra(NEW_KEY,extra);
+            startActivity(intent);
+        }
     }
 
     public void ClickNow(View view) {
-        String timeStamp = new SimpleDateFormat("yyyy/MM/dd HH:mm").format(Calendar.getInstance().getTime());
-
-        Toast.makeText(this, timeStamp,Toast.LENGTH_SHORT).show();
+        GetNowTime();
+        makeToast("已更新時間為\n" + timeMessage);
     }
 
     public void ClickDate(View view) {
+        TimePickerFragment Time = new TimePickerFragment();
+        Time.show(getSupportFragmentManager(),"timePicker");
         DatePickerFragment Date = new DatePickerFragment();
         Date.show(getSupportFragmentManager(),"datePicker");
     }
 
-    public void ClickTime(View view) {
-        TimePickerFragment Time = new TimePickerFragment();
-        Time.show(getSupportFragmentManager(),"timePicker");
-    }
-
+    @SuppressLint("DefaultLocale")
     public void processDatePickerResult(int year, int month, int day){
-        String month_string = Integer.toString(month+1); // 月份使從0開始的，要加1
-        String day_string = Integer.toString(day);
-        String year_string = Integer.toString(year);
-        String dateMessage = (year_string + "/" + month_string + "/" + day_string);//響應信息
-        Toast.makeText(this, dateMessage, Toast.LENGTH_SHORT).show(); // 響應方法
+        month_string = String.format("%02d", month + 1); // 月份使從0開始的，要加1
+        day_string = String.format("%02d", day);
+        year_string = String.format("%04d", year);
     }
 
+    @SuppressLint("DefaultLocale")
     public void processTimePickerResult(int hour, int minute){
-        String hour_string = Integer.toString(hour);
-        String minute_string = Integer.toString(minute);
-        String timeMessage = (hour_string + ":" + minute_string);//響應信息
-        Toast.makeText(this, timeMessage, Toast.LENGTH_SHORT).show(); // 響應方法
+        hour_string = String.format("%02d",hour);
+        minute_string = String.format("%02d", minute);
+        second_string = "00";
+        timeMessage = year_string + "/" + month_string + "/" + day_string + " "
+                + hour_string + ":" + minute_string + ":" + second_string;
+        mTextviewTime.setText(timeMessage);
+        makeToast("已更新時間為\n" + timeMessage);
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    public void GetNowTime(){
+        year_string = new SimpleDateFormat("yyyy").format(Calendar.getInstance().getTime());
+        month_string = new SimpleDateFormat("MM").format(Calendar.getInstance().getTime());
+        day_string = new SimpleDateFormat("dd").format(Calendar.getInstance().getTime());
+        hour_string = new SimpleDateFormat("HH").format(Calendar.getInstance().getTime());
+        minute_string = new SimpleDateFormat("mm").format(Calendar.getInstance().getTime());
+        second_string = new SimpleDateFormat("ss").format(Calendar.getInstance().getTime());
+        timeMessage = year_string + "/" + month_string + "/" + day_string + " "
+                + hour_string + ":" + minute_string + ":" + second_string;
+        mTextviewTime.setText(timeMessage);
+    }
+
+    public void makeToast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
 }
